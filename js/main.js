5c4f100bfea9eab163f4c6127c9dddd11a2e1d3c
Ihor <lucky210788@gmail.com>
2025-02-25 14:37:33 +0200

    add new land

--- js/main.js
+++ js/main.js
@@ -1,10 +1,43 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const navIcon = document.querySelector('.menu-icon');
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.querySelector(".cta__btn");
+    const body = document.body;
+    const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+    const isPortrait = window.matchMedia("(orientation: portrait)").matches;
 
-    if (navIcon) {
-        navIcon.addEventListener('click', function() {
-            navIcon.classList.toggle('open');
-            document.querySelector('body').classList.toggle('hidden');
-        });
+    if (isTouchDevice && isPortrait) {
+        if (button) {
+            button.addEventListener("click", (event) => {
+                event.preventDefault();
+
+                body.classList.add("hover-mob");
+
+                setTimeout(() => {
+                    window.location.href = button.getAttribute("href");
+                }, 700);
+            });
+        }
+    } else if (isTouchDevice) {
+        if (button) {
+            button.addEventListener("click", (event) => {
+                event.preventDefault();
+
+                body.classList.add("hover");
+
+                setTimeout(() => {
+                    window.location.href = button.getAttribute("href");
+                }, 700);
+            });
+        }
+    }
+    else {
+        if (button) {
+            button.addEventListener("mouseenter", () => {
+                body.classList.add("hover");
+            });
+
+            button.addEventListener("mouseleave", () => {
+                body.classList.remove("hover");
+            });
+        }
     }
 });
